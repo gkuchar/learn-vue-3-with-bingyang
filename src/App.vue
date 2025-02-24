03fdbb7274ee8b9202588b81ebb6ff140433367c
Griffin Kuchar <griffin.kuchar@gmail.com>
2025-02-24 08:18:49 -0600

    mustache not

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    hello view
-  </div>
+  <h1>{{ message }}</h1>
+  <hr />
+  <h1>number: {{ number }}</h1>
+  <hr/>
+  <h1>double it = {{ doubleNumber(number) }}</h1>
+  <hr/>
+  <h1>{{ number > 150?'num bigger than 150':'less than 150' }}</h1>
+  <hr/>
+  <h1 v-text="number"></h1>
+  <h1>{{ rawHtml }}</h1>
+  <h1 v-html="rawHtml"></h1>
 </template>
 
 <script setup>
+  let message = 'hello view'
+  let number = 50
+
+  function doubleNumber(num) {
+    return num * 2
+  }
+
+  let rawHtml = '<span style="color: red">This should be red</span>'
 
 </script>
 
